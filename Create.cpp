63ddccb21ead17bb19080241b259c768e75563ab
Darya Martynova <daria.mart@bk.ru>
2021-03-18 12:28:16 +0300

    )

--- Create.cpp
+++ Create.cpp
@@ -6,7 +6,7 @@
 
 CVector* create(string str, map<string, Fabric*> factories) {
 	vector<string> vec;
-	size_t t= 0;
+	size_t t = 0;
 	string res = "";
 
 	while (t < str.size())
@@ -23,17 +23,18 @@ CVector* create(string str, map<string, Fabric*> factories) {
 		t++;
 	}
 	vec.push_back(res);
-	int n = vec[2][0]-48;
+	int n = vec[2][0] - 48;
 	vector<double> z(n);
-	
+
 	for (int i = 0; i < n; ++i)
 	{
-		z[i] = double(vec[i+3][0])-48;
+		z[i] = double(vec[i + 3][0]) - 48;
 	}
-	if (vec[0][0] == 'H') {
-		return factories["Hori"]->Create(vec[1], z, n);
+	auto search = factories.find(vec[0]);
+	if (search == factories.end()) {
+		std::cout << "Not found\n";
+		throw - 1;
 	}
-	else {
-		return factories["Vert"]->Create(vec[1], z, n);
-	}
+	return factories[vec[0]]->Create(vec[1], z, n);
 }
+
